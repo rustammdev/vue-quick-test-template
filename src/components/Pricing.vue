595082e0d828e08b540b12857984fc9b57180797
Rustam Nuriddinov <rustamm.nuriddinov@gmai.com>
2024-08-30 12:28:09 +0500

    pricing complate

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -6,25 +6,28 @@
         Pricing
       </h2>
       <p
-        class="text-center text-[18px]led leading-[22px] tracking-tight text-[#010D3E] mt-4 w-[80%] mx-auto font-medium">
+        class="text-center text-[18px]led leading-[22px] tracking-tight text-[#010D3E] mt-4 w-[80%] mx-auto font-medium max-w-lg">
         Freee forever. Upgrade for unlimited tasks, better sucurity,
         and excluciv feature.
       </p>
-      <div class="py-8 flex flex-col">
+
+      <div
+        class="flex flex-col items-center lg:flex-row lg:items-end">
+        <!-- Pricing card -->
         <div
           v-for="plan in pricing"
           :key="plan.plan"
           :class="
             twMerge(
-              'p-10 border shadow-[0_7px_14px_#eaeaea] rounded-xl my-8 mx-4 sm:border border-[#F1F1F1]',
-              plan.inverse === true && 'bg-black text-white'
+              'p-10 border shadow-[0_7px_14px_#eaeaea] rounded-3xl my-6 mx-4 sm:border border-[#F1F1F1] max-w-sm w-full',
+              plan.active === true && 'bg-black text-white'
             )
           ">
           <h2
             :class="
               twMerge(
                 'text-xl py-1 font-bold text-black/70 mb-4',
-                plan.inverse && 'text-white/50',
+                plan.active && 'text-white/50',
                 plan.popular &&
                   'bg-gradient-to-b from-[#b921bc] via-[#bed76d] to-[#2caaee] text-transparent bg-clip-text'
               )
@@ -41,13 +44,15 @@
               :class="
                 twMerge(
                   'tracking-tight font-bold text-black/50',
-                  plan.inverse && 'text-white/50'
+                  plan.active && 'text-white/50'
                 )
               "
               >/month</span
             >
           </div>
-          <p class="font-normal mt-2">{{ plan.description }}</p>
+          <p class="font-normal mt-4">
+            {{ plan.description }}
+          </p>
           <ul class="flex flex-col gap-2 mt-6">
             <li
               v-for="feature in plan.features"
@@ -60,12 +65,13 @@
           <button
             :class="
               twMerge(
-                'bg-black hover:bg-black/80 w-full text-white px-4 py-2 font-medium rounded-lg tracking-tight inline-flex items-center justify-center mt-[30px]',
-                plan.inverse === true &&
-                  'bg-white text-black hover:bg-slate-700'
+                'bg-black hover:bg-black/80 w-full text-white px-4 py-2 font-medium rounded-lg tracking-tight inline-flex items-center justify-center mt-[30px] transition ease-in-out delay-100',
+                plan.popular &&
+                  'bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-700 hover:to-orange-400  ',
+                plan.active === true &&
+                  'bg-white text-black hover:bg-neutral-300'
               )
-            "
-            :disabled="plan.plan === 'Free'">
+            ">
             {{ plan.buttonText }}
           </button>
         </div>
@@ -84,7 +90,7 @@ const pricing = [
     description: "Get started with basic features.",
     buttonText: "Get started for free",
     popular: false,
-    inverse: false,
+    active: false,
     features: [
       "Access to basic Q&A features",
       "Limited AI-powered analysis",
@@ -96,7 +102,7 @@ const pricing = [
     plan: "One Time",
     price: "$99",
     description: "One-time payment for extended features.",
-    inverse: false,
+    active: false,
     popular: false,
     buttonText: "Let's choose lifetime Plan",
     features: [
@@ -109,7 +115,7 @@ const pricing = [
   {
     plan: "Business",
     price: "$299",
-    inverse: true,
+    active: true,
     popular: true,
     description: "For businesses that need advanced capabilities.",
     buttonText: "Business Plan",
@@ -124,17 +130,4 @@ const pricing = [
 ];
 </script>
 
-<style>
-.text-gradient {
-  background: #ac3bcf;
-  background: linear-gradient(
-    to right,
-    #ac3bcf 0%,
-    #7bff77 50%,
-    #cf1512 100%
-  );
-  -webkit-background-clip: text;
-  background-clip: text;
-  color: transparent;
-}
-</style>
+<style></style>
